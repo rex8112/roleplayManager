dfa5e83d9b82ee2d0c85b2d991d4e8cc02450b26
Riley Winkler <rex8112@gmail.com>
2022-01-25 04:25:21 -0600

    Add information controlling methods

--- models/character.js
+++ models/character.js
@@ -30,4 +30,54 @@ class Character {
             throw new Error('Invalid classification');
         }
     }
+
+    /**
+     * Removes information from a character.
+     * @param {number} id The id of the information to retrieve.
+     */
+    removeInformation(id) {
+        this.publicInformation.delete(id);
+        this.privateInformation.delete(id);
+        this.gmInformation.delete(id);
+    }
+
+    /**
+     * Set the clearance classification of a character's information.
+     * @param {number} id The id of the information to change.
+     * @param {string} classification The classification of the information. (public, private, gm)
+     * @returns {boolean} Whether or not the information was changed.
+     */
+    setInformationClassification(id, classification) {
+        let information = null;
+
+        // Find the information object
+        if (this.publicInformation.has(id)) {
+            information = this.publicInformation.get(id);
+            this.publicInformation.delete(id);
+        } else if (this.privateInformation.has(id)) {
+            information = this.privateInformation.get(id);
+            this.privateInformation.delete(id);
+        } else if (this.gmInformation.has(id)) {
+            information = this.gmInformation.get(id);
+            this.gmInformation.delete(id);
+        } else {
+            return false;
+        }
+
+        // Add the information to the new classification
+        if (classification === 'public') {
+            this.publicInformation.set(id, information);
+        } else if (classification === 'private') {
+            this.privateInformation.set(id, information);
+        } else if (classification === 'gm') {
+            this.gmInformation.set(id, information);
+        } else {
+            throw new Error('Invalid classification');
+        }
+        return true;
+    }
 }
+
+module.exports = {
+    Character,
+};
